dee9eff31f7c6a7257877d032011b35b2cb08159
Rotichtonny <tonnyrotich.tr@gmail.com>
2020-10-17 02:21:10 +0300

    Initial Commit

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace UserService.Controllers
+{
+    [Authorize] //Attribute for authorization - all the actions need to be authenticated.
+    public class UsersController : ApiController
+    {
+        public IEnumerable<User> Get()
+        {
+            //Instance of UserDB entities.
+            using (UserDBEntities entities = new UserDBEntities())
+            {
+                //Return List of Users objects.
+                return entities.Users.ToList();
+            }
+        }
+    }
+}
